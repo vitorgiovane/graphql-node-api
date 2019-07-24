95b9a25d5e671c6068ae759b273c15ad1533ff62
Vitor <vitorgpalves@gmail.com>
2019-07-23 23:32:46 -0300

    feature: implementa a primeira query com Graphql

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as express from "express"
-
+import * as graphqlHTTP from "express-graphql"
+import schema from "./graphql/schema"
 class App {
   public express: express.Application
 
@@ -10,16 +11,11 @@ class App {
 
   private middleware(): void {
     this.express.use(
-      "/hello",
-      (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        res.send({
-          hello: "Hello, World!!!"
-        })
-      }
+      "/graphql",
+      graphqlHTTP({
+        schema: schema,
+        graphiql: process.env.NODE_ENV === "development"
+      })
     )
   }
 }
